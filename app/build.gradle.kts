5a555352777be8eb0e6283257245e4804ddfa6ef
angamer234k <tima.zhigachev@inbox.ru>
2026-09-25 14:38:42 +0300

    Restore gson dep; keep 2.9.2 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,9 +71,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
     implementation("androidx.activity:activity-ktx:1.9.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
